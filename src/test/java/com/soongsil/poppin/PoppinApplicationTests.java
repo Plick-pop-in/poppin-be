c51ab4e49e871226b29a385f571737ebbbe6c613
Yerin Song <yrsong0915@gmail.com>
2024-04-04 19:56:19 +0900

    setting : be project init

---
+++ src/test/java/com/soongsil/poppin/PoppinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soongsil.poppin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoppinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
