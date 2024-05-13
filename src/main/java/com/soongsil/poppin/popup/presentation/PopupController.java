9f66486eac5011b2695282129c869654adc0e1eb
Yerin Song <yrsong0915@gmail.com>
2024-05-13 22:41:20 +0900

    feat: 메인페이지 좋아요 top3 불러오기 기능 추가

--- src/main/java/com/soongsil/poppin/popup/presentation/PopupController.java
+++ src/main/java/com/soongsil/poppin/popup/presentation/PopupController.java
@@ -4,6 +4,7 @@ package com.soongsil.poppin.popup.presentation;
 import com.soongsil.poppin.global.response.ResponseDto;
 import com.soongsil.poppin.popup.application.PopupSearchService;
 import com.soongsil.poppin.popup.application.response.ImgUrlList;
+import com.soongsil.poppin.popup.application.response.TopPopup;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +19,19 @@ import java.util.List;
 public class PopupController {
     private final PopupSearchService popupSearchService;
 
+    //메인페이지 랜덤이미지(5개) 불러오기
     @GetMapping("/random-images")
     public ResponseDto<ImgUrlList> getRandomPopupImageUrls() {
             ImgUrlList imgUrlList = popupSearchService.getRandomPopupImageUrls(5); // 5개의 랜덤 이미지 URL 가져오기
             return ResponseDto.map(HttpStatus.OK.value(),"랜덤 팝업 이미지 5개 불러오기 성공", imgUrlList);
     }
+
+    //메인페이지 좋아요 top3 불러오기
+    @GetMapping("/top3")
+    public ResponseDto<List<TopPopup>> getTop3PopupsWithMostLikes() {
+        List<TopPopup> top3Popups = popupSearchService.getTop3PopupsWithMostLikes();
+        return ResponseDto.map(HttpStatus.OK.value(), "좋아요 TOP3 팝업 정보 불러오기 성공", top3Popups);
+    }
+
+
 }
