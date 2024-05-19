a44be3622edc4d4652ee8e48540ae68f86f63cf4
hyo-limilimee <1001lily@naver.com>
2024-05-20 01:20:09 +0900

    refactor: getter, setter annotation으로 대체

--- src/main/java/com/soongsil/poppin/userchat/application/request/UserChatRequest.java
+++ src/main/java/com/soongsil/poppin/userchat/application/request/UserChatRequest.java
@@ -1,13 +1,10 @@
 package com.soongsil.poppin.userchat.application.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserChatRequest {
     private Long userId;
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
 }
