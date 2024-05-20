e94a4c0bc6b3e8ffd0a3d045ece27ec6facdc629
hyo-limilimee <1001lily@naver.com>
2024-05-20 13:50:27 +0900

    fix: UserChatRepository 쿼리 수정

--- src/main/java/com/soongsil/poppin/userchat/domain/UserChatRepository.java
+++ src/main/java/com/soongsil/poppin/userchat/domain/UserChatRepository.java
@@ -11,9 +11,13 @@ import org.springframework.data.repository.query.Param;
 public interface UserChatRepository extends JpaRepository<UserChat, Long> {
 
     @Query("SELECT p.popupName " +
-            "FROM Popup p " +
-            "JOIN UserChat u ON u.popup = p " +
-            "WHERE u.member.userId = :userId")
+            "FROM UserChat uc " +
+            "JOIN Popup p ON uc.popup.popupId = p.popupId " +
+            "WHERE uc.member.userId = :userId " +
+            "AND uc.popup.popupId IN ( " +
+            "    SELECT uc2.popup.popupId " +
+            "    FROM UserChat uc2 " +
+            "    WHERE uc2.member.userId = :userId)")
 
     Page<String[]> findUserChatListByUserId(@Param("userId") Long userId, Pageable pageable);
 }
