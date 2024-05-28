c1fda22c3df70a787bfc4b4d6e1cbb8638a58fbc
Yerin Song <yrsong0915@gmail.com>
2024-05-28 20:29:04 +0900

    fix: 메인페이지 top3 팝업불러올때 기간과 좋아요 개수별로 불러오는 기능 오류 수정

--- src/main/java/com/soongsil/poppin/popup/domain/PopupRepository.java
+++ src/main/java/com/soongsil/poppin/popup/domain/PopupRepository.java
@@ -11,13 +11,22 @@ import java.util.List;
 @Repository
 public interface PopupRepository extends JpaRepository<Popup, Long> {
 
-    @Query("SELECT i.popupImageUrl, p.popupName, " +
-            "CONCAT(FORMAT(p.popupStartDate, 'yyyy-MM-dd'), ' - ', FORMAT(p.popupEndDate, 'yyyy-MM-dd')) AS popupPeriod, " +
-            "(SELECT COUNT(h) FROM Heart h WHERE h.popup = p) AS likeCount " +
+    // 좋아요가 가장 많은 상위 3개 팝업 ID 가져오기
+    @Query("SELECT p.popupId " +
             "FROM Popup p " +
-            "JOIN p.popupImages i " +
-            "GROUP BY p.popupId, i.popupImageUrl, p.popupName, p.popupStartDate, p.popupEndDate " +
-            "ORDER BY likeCount DESC")
+            "JOIN Heart h ON p.popupId = h.popup.popupId " +
+            "GROUP BY p.popupId " +
+            "ORDER BY COUNT(h) DESC " +
+            "LIMIT 3")
+    List<Long> findTop3PopupIdsWithMostLikes();
+
+    // 메인페이지 좋아요 top3 가져오는 쿼리
+    @Query("SELECT p.popupId, i.popupImageUrl, p.popupName, " +
+            "CONCAT(FUNCTION('DATE_FORMAT', p.popupStartDate, '%Y-%m-%d'), ' - ', FUNCTION('DATE_FORMAT', p.popupEndDate, '%Y-%m-%d')) AS popupPeriod, " +
+            "(SELECT COUNT(*) FROM Heart h WHERE h.popup = p) AS likeCount " +
+            "FROM Popup p " +
+            "LEFT JOIN p.popupImages i " +
+            "GROUP BY p.popupId, i.popupImageUrl, p.popupName, p.popupStartDate, p.popupEndDate ")
     Page<Object[]> findTop3ByOrderByLikeCountDesc(Pageable pageable);
 
     //id에 해당하는 값 가져오는 쿼리
