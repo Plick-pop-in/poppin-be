4f545c6b56c1236334586799d9f16884323348bc
hyo-limilimee <1001lily@naver.com>
2024-05-29 04:11:16 +0900

    feat: 팝업 별 참여한 인원 수 가져오는 쿼리 추가

--- src/main/java/com/soongsil/poppin/popup/domain/LiveRepository.java
+++ src/main/java/com/soongsil/poppin/popup/domain/LiveRepository.java
@@ -3,6 +3,7 @@ package com.soongsil.poppin.popup.domain;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,5 +14,11 @@ public interface LiveRepository extends JpaRepository<Popup,Long> {
     @Query("SELECT p" +
             " FROM Popup p " +
             "WHERE :currentDate < p.popupEndDate")
-    List<Popup> findDateBeforeEndDate(LocalDateTime currentDate, Pageable pageable);
+    List<Popup> findPopupBeforeEndDate(LocalDateTime currentDate, Pageable pageable);
+
+    // 팝업 별 참여한 인원 수 가져오기
+    @Query("SELECT COUNT(uc.userId) " +
+            "FROM UserChat uc " +
+            "GROUP BY uc.popupId")
+    long getJoinedPeopleCnt(@Param("popuupId") Long popupId);
 }
