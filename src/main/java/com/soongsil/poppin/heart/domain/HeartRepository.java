bc1ca84cb153ee99864be1c42c336e1cae783f3e
Yerin Song <yrsong0915@gmail.com>
2024-05-28 20:28:23 +0900

    feat: 메인페이지 좋아요 top3 관련 좋아요 개수 쿼리

--- src/main/java/com/soongsil/poppin/heart/domain/HeartRepository.java
+++ src/main/java/com/soongsil/poppin/heart/domain/HeartRepository.java
@@ -1,13 +1,16 @@
 package com.soongsil.poppin.heart.domain;
 
 
-import com.soongsil.poppin.popup.domain.Popup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HeartRepository extends JpaRepository<Heart, Long> {
+    // 메인페이지 사용 팝업별 heart 개수 count
+    @Query("SELECT COUNT(h) FROM Heart h WHERE h.popup.id = ?1")
+    Long countHeartByPopup(Long popupId);
+
     //heart 개수 Count
     @Query(value = "SELECT count(*) FROM Heart WHERE popup_id = :popupId", nativeQuery = true)
     Long countHeartById(Long popupId);
