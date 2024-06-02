82cd8b143d15310822375b8bd286b3fb771880da
Yerin Song <yrsong0915@gmail.com>
2024-06-03 01:27:38 +0900

    feat: 메시지 dto

---
+++ src/main/java/com/soongsil/poppin/userchat/application/ChatMessage.java
@@ -0,0 +1,11 @@
+package com.soongsil.poppin.userchat.application;
+
+import java.awt.*;
+
+public class ChatMessage {
+
+    private TrayIcon.MessageType type;
+    private String content;
+    private String sender;
+
+}
