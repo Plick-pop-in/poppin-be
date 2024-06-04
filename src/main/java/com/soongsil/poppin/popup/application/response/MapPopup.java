b067a714765cc4eccbed9637db33aafdf6781109
hyo-limilimee <1001lily@naver.com>
2024-06-04 23:53:40 +0900

    feat: map popup response 응답 필드 추가

--- src/main/java/com/soongsil/poppin/popup/application/response/MapPopup.java
+++ src/main/java/com/soongsil/poppin/popup/application/response/MapPopup.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class MapPopup {
+    private final long popupId;
     private final String popupName;
     private final String popupLocation;
     private final String popupCity;
