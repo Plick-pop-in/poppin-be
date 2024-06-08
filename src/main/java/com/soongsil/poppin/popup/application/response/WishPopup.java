0ab8d82baffbd09b71a337e98af494741d3bed35
GimHaLim <halim7568@gmail.com>
2024-06-08 18:21:05 +0900

    feat: 마이페이지 찜 목록 api

---
+++ src/main/java/com/soongsil/poppin/popup/application/response/WishPopup.java
@@ -0,0 +1,12 @@
+package com.soongsil.poppin.popup.application.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class WishPopup {
+    private final Long popupId;
+    private final String popupImage;
+    private final String popupName;
+}
